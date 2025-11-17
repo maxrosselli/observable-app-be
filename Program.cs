fd924c5425c638d52aed161375dd4626c7efa856
Massimo Rosselli <m.rosselli@reply.it>
2025-11-17 12:53:52 +0100

    Abilitato swagger

--- Program.cs
+++ Program.cs
@@ -33,7 +33,15 @@ builder.Services.AddScoped<IServiceBusPublisher>(provider =>
     return new ServiceBusPublisher(client);
 });
 
+// 🔹 Aggiungi Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 
 app.UseAuthorization();
 
